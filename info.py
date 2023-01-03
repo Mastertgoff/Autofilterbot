ae76a52ce111ca7dd437d7fc71c36ee021c93303
Mastertgoff <121784621+Mastertgoff@users.noreply.github.com>
2023-01-03 19:10:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,6 +41,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
+BOT_START_TIME = time()
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
